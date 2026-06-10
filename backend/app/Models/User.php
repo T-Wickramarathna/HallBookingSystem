884fb6ca3c4e75b8cef407dce18ea082e1976d3d
Theekshana Wickramarathna <thwicramarathna@gmail.com>
2026-06-11 02:53:38 +0530

    Configure user fillables and register admin middleware

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['first_name', 'last_name', 'email', 'password', 'mobile', 'designation', 'role'])]
+#[Fillable(['first_name', 'last_name', 'email', 'password', 'mobile', 'designation', 'role', 'is_active'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -26,6 +26,7 @@ class User extends Authenticatable
         'mobile',
         'designation',
         'role',
+        'is_active',
     ];
 
     /**
--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'manager' => \App\Http\Middleware\ManagerMiddleware::class,
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
